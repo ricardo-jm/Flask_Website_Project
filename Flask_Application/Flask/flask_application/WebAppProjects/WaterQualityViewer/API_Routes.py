e3061c99bb862f6995bdeccc2c25dad50ec9d263
ricardo-jm <122681314+ricardo-jm@users.noreply.github.com>
2024-12-02 19:19:35 +0000

    Update API_Routes.py XSS water quality

--- Flask_Application/Flask/flask_application/WebAppProjects/WaterQualityViewer/API_Routes.py
+++ Flask_Application/Flask/flask_application/WebAppProjects/WaterQualityViewer/API_Routes.py
@@ -1,5 +1,6 @@
 from flask_application.WebAppProjects.WaterQualityViewer import functionsWaterQual, DBQueriesWaterQuality
 from flask import render_template, Blueprint, request
+from werkzeug.utils import escape  # Import escape function
 
 sbcWaterQualityAPI_BP = Blueprint('sbcWaterQualityAPI_BP', __name__,
                         template_folder='templates',
@@ -9,4 +10,4 @@ sbcWaterQualityAPI_BP = Blueprint('sbcWaterQualityAPI_BP', __name__,
 @sbcWaterQualityAPI_BP.route("getbeachhistory", methods=['GET'])
 def getWaterQualityHistory():
     beachName = str(request.args.get("beachName"))
-    return DBQueriesWaterQuality.getBeachResults(beachName)
+    return DBQueriesWaterQuality.getBeachResults(escape(beachName))
